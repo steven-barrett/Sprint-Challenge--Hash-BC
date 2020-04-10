b0fcb6ce9ca03e84aa5bfc31da796a2aaa9c53e5
Steve Barrett <steven122185@gmail.com>
2020-04-10 12:13:26 -0400

    Finished ex1

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -13,7 +13,25 @@ def get_indices_of_item_weights(weights, length, limit):
     YOUR CODE HERE
     """
 
-    return None
+    answer = []
+    for index, item in enumerate(weights):
+        weight_left = limit - item
+        # print('looking for key', weight_left,
+        hash_table_retrieve(ht, weight_left)
+        if hash_table_retrieve(ht, weight_left) != None:
+            # print('found item')
+            if hash_table_retrieve(ht, weight_left) > index:
+                answer.append(hash_table_retrieve(ht, weight_left))
+                answer.append(index)
+            else:
+                answer.append(index)
+                answer.append(hash_table_retrieve(ht, weight_left))
+        hash_table_insert(ht, item, index)
+    # print(answer)
+    if len(answer) == 2:
+        return tuple(answer)
+    else:
+        return None
 
 
 def print_answer(answer):
