7bcf40b8d5516f40c029883b6e45ad602eb50b7c
Steve Barrett <steven122185@gmail.com>
2020-04-10 13:14:54 -0400

    Finished ex1, not tested yet due to my miner still running

--- hashtables/ex2/ex2.py
+++ hashtables/ex2/ex2.py
@@ -19,5 +19,36 @@ def reconstruct_trip(tickets, length):
     """
     YOUR CODE HERE
     """
+    hashtable = HashTable(length)
+    route = [None] * length
+    # print(tickets)
+    # insert tickets in ht /// source and destination from class
+    for t in tickets:
+        hash_table_insert(hashtable, t.source, t.destination)
 
-    pass
+    # count for iteration
+    count = 0
+    # current/start location
+    key = "NONE"
+    # while the last element in the route list is None, it means we haven't hit the end, so keep going
+    while route[-1] is None:
+        # find where the next flight is, store it here. The return of the hash_retrieve is the key of the next flight
+        next_flight = hash_table_retrieve(hashtable, key)
+        # add the flight to the route list
+        route[count] = next_flight        
+        # increment in order to keep moving through list
+        count += 1
+        # change key to where you're at on the list now
+        key = next_flight
+
+    print(route)
+    return route
+
+
+ticket_1 = Ticket("NONE", "PDX")
+ticket_2 = Ticket("PDX", "DCA")
+ticket_3 = Ticket("DCA", "NONE")
+
+tickets = [ticket_1, ticket_2, ticket_3]
+
+reconstruct_trip(tickets, 3)
